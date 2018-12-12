e3344c5ea196d04804dd9fe4b2c0cb9f3e9958e8
067-francisco-s@sapo.corppt.com <fsfregalado@ua.pt>
2018-12-12 21:14:48 +0000

    removed some unused code 2.0

--- src/js/services/articles.js
+++
@@ -1,5 +0,0 @@
-import {ENDPOINT_ARTICLE} from "../constants/services";
-
-export function fetchAll() {
-    return fetch(ENDPOINT_ARTICLE).then(response => response.json(), );
-}
--- src/js/services/users.js
+++
@@ -1,5 +0,0 @@
-import {ENDPOINT_USER} from "../constants/services";
-
-export function fetchAll() {
-    return fetch(ENDPOINT_USER).then(response => response.json(), );
-}
